e870cc824afe3cef2aa0694b471be094d808b04c
Saurabh Solanke <35625425+saurabhsolanke@users.noreply.github.com>
2021-09-30 11:38:26 +0530

    v-1.0.0

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,41 @@
+import {Task} from './Task'
+
+export const TASKS: Task[] = [
+    {
+        id:1,
+        text: 'Doctors Appointment',
+        day: 'May 5th at 2:30pm',
+        reminder: true,
+        
+    },
+    {
+        id:2,
+        text: 'Gym Appointment',
+        day: 'May 6th at 6:30am',
+        reminder: true,
+    },
+    {
+        id:3,
+        text: 'shopping',
+        day: 'May 7th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id:4,
+        text: 'Doctors Appointment',
+        day: 'May 8th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id:5,
+        text: 'Mum Dad Anniversary',
+        day: 'May 9th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id:5,
+        text: 'Birthday',
+        day: 'May 10th at 2:30pm',
+        reminder: true,
+    },
+]
